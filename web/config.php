de87808e560ec2753707a8fd8eceac59ef185b00
xiaomingwtdywt <62534998+xiaomingwtdywt@users.noreply.github.com>
2020-03-23 15:27:25 +0800

    Update config.php

--- web/config.php
+++ web/config.php
@@ -1,10 +1,10 @@
 <?php
 $gl=(isset($_COOKIE['gl']) && $_COOKIE['gl'])?$_COOKIE['gl']:'US';
 define('ROOT_PART', Root_part());
-define('APIKEY', 'AIzaSyBmYQPhWRuVcFG8XNO-WDBFLZpZtIkKlbY');
+define('APIKEY', 'AIzaSyAMgJmdzLqNmBidFkNyFQOTFC-zPNr4V24');
 define('GJ_CODE', $gl);
 define('SITE_NAME', 'Hello World!');
 define('TITLENAME', 'Hello World!');
 define('EN2DEKEY', 'AnQUP1XDEF87ewemhuYG35e67CtzDSh4');
-define('EMAIL', 'None@gmail.com');
+define('EMAIL', '2871515528@qq.com');
 ?>
